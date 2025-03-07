70d27abdd9c1c7fcd40384e1142bf5e62f30fca1
Prabin Bhatt <prabin@portpro.io>
2025-03-07 12:57:31 +0545

    merge info

--- components/reviews.tsx
+++ components/reviews.tsx
@@ -24,8 +24,8 @@ export default function ReviewPage({ pullRequests }: { pullRequests: any[] }) {
                             <div className="flex items-center gap-4">
                                 <Link href={`https://github.com/${pr?.repository?.owner}/${pr?.repository?.name}/pull/${pr?.pullId}`}><h1 className={`${title()} text-primary underline`}>{`#${pr.pullId}`}</h1></Link>
                                 <div className="flex flex-col gap-2">
-                                    <Chip color='default' size="sm">{pr?.metadata?.base}</Chip><ArrowLeft size={16} /><Chip color='default' size="sm">{pr?.metadata?.base}</Chip>
-                                    <Chip color={pr.status === "CLOSED" ? "danger" : "primary"} size="sm">{pr.status}</Chip> by <strong>{pr?.metadata?.merged_by?.login}</strong>
+                                    <div className="flex items-center gap-4"><Chip color='default' size="sm">{pr?.metadata?.base}</Chip><ArrowLeft size={16} /><Chip color='default' size="sm">{pr?.metadata?.base}</Chip></div>
+                                    <div className="flex items-center gap-4"><Chip color={pr.status === "CLOSED" ? "danger" : "primary"} size="sm">{pr.status}</Chip> by <strong>{pr?.metadata?.merged_by?.login}</strong></div>
                                 </div>
                             </div>
 
