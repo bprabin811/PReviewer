cf730c7e2bfb2a4c77f2a753d93bbf7929151cf0
Prabin Bhatt <prabin@portpro.io>
2025-03-09 20:25:14 +0545

    update: langing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { subtitle, title } from "@/components/primitives";
-import { GitPullRequest } from "lucide-react";
-import { useSession } from "next-auth/react";
+import { Button } from "@heroui/button";
+import { signIn, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 
 export default function Home() {
@@ -12,31 +12,25 @@ export default function Home() {
   }
 
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="max-w-xl flex flex-col items-center gap-4 text-center">
-        <GitPullRequest size={80} className="text-default"/>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 bg-[url('/background_light.svg')] dark:bg-[url('/background_dark.svg')] bg-cover bg-center">
+      <div className="max-w-xl flex flex-col items-center gap-4 text-center py-16">
         <span className={title()}>Automate&nbsp;</span>
-        <span className={title({ color: "violet" })}>Pull Request Reviews&nbsp;</span>
+        <span className={title({ color: "blue"})}>Pull Request Reviews&nbsp;</span>
         <br />
 
         <span className={title()}>with AI-powered insights.</span>
         <div className={subtitle({ class: "mt-4" })}>
           PReviewer streamlines your code review process by leveraging AI to analyze pull requests, provide feedback, and ensure high-quality contributions—effortlessly.
         </div>
-        {/* <div className="mt-6 flex justify-center gap-4">
-      <Link href="/dashboard" className={buttonStyles({ color: "violet" })}>
-        Get Started
-      </Link>
-      <Link
-        href={siteConfig.links.github}
-        target="_blank"
-        className={buttonStyles({ variant: "outline" })}
-      >
-        <GithubIcon className="mr-2 h-5 w-5" />
-        Star on GitHub
-      </Link>
-    </div> */}
 
+        <div className="flex gap-4">
+        <Button color="primary" variant="flat" onPress={() => signIn()}>
+          Get Started
+        </Button>
+        <Button color="default" variant="ghost" onPress={() => {}}>
+          Watch Demo
+        </Button>
+        </div>
       </div>
     </section>
   );
